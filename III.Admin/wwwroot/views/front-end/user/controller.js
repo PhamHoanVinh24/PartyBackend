5a4a7b73791fc9444e939a0637cf8683a2cdc27f
QUARK23\Admin <TheLight05111998@gmail.com>
2024-02-20 11:19:01 +0700

    fix file mẫu

--- III.Admin/wwwroot/views/front-end/user/controller.js
+++ III.Admin/wwwroot/views/front-end/user/controller.js
@@ -207,7 +207,7 @@ app.controller('index', function ($scope, $rootScope, $compile, dataservice, $fi
     $scope.downloadFile=function() {
         // Tạo một phần tử a để tạo ra một liên kết tới tệp Word
         var link = document.createElement("a");
-        link.href = "/files/Mẫu 2- KNĐ năm 2023 2.docx"; // Đặt đường dẫn đến tệp Word
+        link.href = "/files/Mẫu 2- KNĐ năm 2023.docx"; // Đặt đường dẫn đến tệp Word
         link.download = "Mẫu 2- KNĐ năm 2023.docx"; // Đặt tên cho tệp khi được tải xuống
         // Kích hoạt sự kiện nhấp vào liên kết
         link.click();
